2684c5f155378f807364e2dab0aec48284df8845
Kozlyakov Maxim <maxim.kozlyakov@gmail.com>
2018-02-21 11:01:13 +0300

    add travis-ci

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build Status](https://travis-ci.org/maximjs/project-lvl4-s211.svg?branch=master)](https://travis-ci.org/maximjs/project-lvl4-s211)
+
+Realtime chat.
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
+// const path = require('path');
+// const webpack = require('webpack');
 
 export default () => ({
   entry: {
