92602dba6fd530104d3307f19c4d50c0355072f8
Kozlyakov Maxim <maxim.kozlyakov@gmail.com>
2018-02-28 13:45:57 +0300

    bug fix

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import Channels from '../containers/Channels';
 
-class Main extends React.Component {
-  render() {
-    return (
-      <div className = "row">
-        <div className = "col-md-3">
-          <h4>Channels:</h4>
-          <Channels />
-        </div>
-        <div className = "col-md-9">
-          <div className = "row">
-            <div className = "col-md jumbotron">
-              <p>Lorem ipsum doloret yuio ffghgf fdfgfhf tdhhfd hfdhd</p>
-            </div>
+const Main = () =>
+  (
+    <div className="row">
+      <div className="col-md-3">
+        <h4>Channels:</h4>
+        <Channels />
+      </div>
+      <div className="col-md-9">
+        <div className="row">
+          <div className="col-md jumbotron">
+            <p>Lorem ipsum doloret yuio ffghgf fdfgfhf tdhhfd hfdhd</p>
           </div>
-          <div className = "row">
-            <div className = "col-md">
-              <div className="form-group">
-                <input type="text" required className="form-control" placeholder="Write a message" />
+        </div>
+        <div className="row">
+          <div className="col-md">
+            <form action="">
+              <div className="row">
+                <div className="col-md-1">
+                  <button type="submit" className="btn btn-primary btn-sm">Send</button>
+                </div>
+                <div className="col-md-11">
+                  <div className="form-group">
+                    <input type="text" required value="" className="form-control" placeholder="Write a message" />
+                  </div>
+                </div>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
 
 export default Main;
--- app/components/Channels.jsx
+++ app/components/Channels.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
-class Channels extends React.Component {
-  render() {
-    const channelsArr = Object.values(this.props.channels);
-    const channels = channelsArr.map(item =>
-      <li key={item.id} className="list-group-item">{item.name}</li>);
-    return (
-      <ul className="list-group">
-        {channels}
-      </ul>
-    );
-  }
-}
+const Channels = (props) => {
+  const channels = Object.values(props.channels);
+  const channelsRender = channels.map(item =>
+    <li key={item.id} className="list-group-item">{item.name}</li>);
+  return (
+    <ul className="list-group">
+      {channelsRender}
+    </ul>
+  );
+};
 
 export default Channels;
